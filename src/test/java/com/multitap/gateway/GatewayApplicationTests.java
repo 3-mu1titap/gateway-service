797ce154e270ff5ecc94f7ede56c631ba12b01a9
kang-subin <tnbin98@gmail.com>
2024-10-25 15:13:57 +0900

    Your commit message here

---
+++ src/test/java/com/multitap/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitap.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
